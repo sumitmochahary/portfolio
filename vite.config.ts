3849e8f3508612bb9fa850bc294119a4661f6b1e
Sumit Mochahary <sumitmochahary5@gmail.com>
2025-12-12 23:09:25 +0530

    Refactor code structure for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,12 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
+          {
+            src: "icons/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
         ],
       },
     }),
